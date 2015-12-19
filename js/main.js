382e77fd0a2a5cc87e827ed9a0c2e34073dfbafa
linyuchien <npr286rx@rocketmail.com>
2015-12-19 17:22:52 +0800

    ver.12

--- js/main.js
+++ js/main.js
@@ -12,28 +12,25 @@ jQuery(document).ready(function(){
         console.log(data);
         // ActiveStart
         console.log(data[i])
-        var temp1 = '<div class="title active"><i class="dropdown icon"></i>';  
-        temp1 += data[0].title+'</div>' ;
-        temp1 += '<div class="content active"><p>' + data[0].content + '</p></div>';
+        var temp1 = '<div class="title active"><i class="dropdown icon"></i>' + data[0].title + '</div>'
+                    +'<div class="content active"><p>' + data[0].content + '</p></div>';
         $(temp1).appendTo("#insert");
         // ActiveEnd
 
-        //限定新消息數量(if最多6則，else不足6則時取前6則)
+        //限定新消息數量(active除外 if最多6則，else不足6則時取前6則)
         if (data.length<=6) {
             for (var i = 1 ; i < data.length ; i++){
                 console.log(data[i])
-                var temp = '<div class="title"><i class="dropdown icon"></i>';  
-                temp += data[i].title+'</div>' ;
-                temp += '<div class="content"><p>' + data[i].content + '</p></div>';
+                var temp = '<div class="title"><i class="dropdown icon"></i>' + data[i].title +'</div>' 
+                            + '<div class="content"><p>' + data[i].content + '</p></div>';  
                 $(temp).appendTo("#insert");
             }
         } 
         else { 
-            for (var i = 1 ; i < 6 ; i++){
+            for (var i = 1 ; i < 7 ; i++){
                 console.log(data[i])
-                var temp = '<div class="title"><i class="dropdown icon"></i>';  
-                temp += data[i].title+'</div>' ;
-                temp += '<div class="content"><p>' + data[i].content + '</p></div>';
+                var temp = '<div class="title"><i class="dropdown icon"></i>' + data[i].title +'</div>' 
+                            + '<div class="content"><p>' + data[i].content + '</p></div>';  
                 $(temp).appendTo("#insert");
             }                    
         }
@@ -46,19 +43,30 @@ jQuery(document).ready(function(){
     var soup = new SpreadsheetSoup(key, myCallback);
 
 
-});
+
 
 //彈跳視窗
-jQuery(document).ready(function(){
-    $('.ui.inverted.button').click(function(){
-        $('.ui.modal.LOL')
-        .modal('show')
-        ;
-    });
+$('.ui.inverted.button').click(function(){
+    $('.ui.modal.LOL')
+    .modal('show')
+    ;
+});
 
-    $('.ui.primary.button').click(function(){
-        $('.ui.modal.HS')
-        .modal('show')
-        ;
-    });
+
+$('.ui.primary.button').click(function(){
+    $('.ui.modal.HS')
+    .modal('show')
+    ;
+});
+
+
+// Logo附著效果
+// $('.garena01')
+//   .transition('scale')
+// ;
+$('.garena01').click(function(){
+  $(this).transition('scale');
+});
+
+
 });
