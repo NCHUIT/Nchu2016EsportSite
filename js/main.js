8f51db8cb3ba599ed55504639c30d106fc038342
linyuchien <npr286rx@rocketmail.com>
2015-12-31 13:39:03 +0800

    ver.22 picture cycle

--- js/main.js
+++ js/main.js
@@ -192,8 +192,6 @@ jQuery(document).ready(function(){
 
 
 
-
-
     //Google Analystics
   (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
   (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
